4114e8d72b6290e88b5f13b0905ae4cf0015454f
Zafazoonia <37117104+Zafazoonia@users.noreply.github.com>
2023-07-04 18:09:13 +0530

    Create XRD_Fit.py
    file for xrd fitting, to be updated

---
+++ XRD_Fit.py
@@ -0,0 +1,54 @@
+import os
+import math
+import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import optimize, signal
+
+def update_spec_from_peaks(spec, model_indicies, peak_widths=(10, 25), **kwargs):
+    x = spec['x']
+    y = spec['y']
+    x_range = np.max(x) - np.min(x)
+    peak_indicies = signal.find_peaks_cwt(y, peak_widths)
+    np.random.shuffle(peak_indicies)
+    for peak_indicie, model_indicie in zip(peak_indicies.tolist(), model_indicies):
+        model = spec['model'][model_indicie]
+        if model['type'] in ['GaussianModel', 'LorentzianModel', 'VoigtModel']:
+            params = {
+                'height': y[peak_indicie],
+                'sigma': x_range / len(x) * np.min(peak_widths),
+                'center': x[peak_indicie]
+            }
+            if 'params' in model:
+                model.update(params)
+            else:
+                model['params'] = params
+        else:
+            raise NotImplemented(f'model {basis_func["type"]} not implemented yet')
+    return peak_indicies
+
+spec = {
+    'x': x,
+    'y': y,
+    'model': [
+        {'type': 'VoigtModel'},
+        {'type': 'VoigtModel'},
+        {'type': 'VoigtModel'},
+        {'type': 'VoigtModel'},
+        {'type': 'GaussianModel'},
+        {'type': 'GaussianModel'},
+        {'type': 'GaussianModel'},
+        {'type': 'GaussianModel'},
+    ]
+}
+
+peaks_found = update_spec_from_peaks(spec, [0, 1, 2, 3, 4, 5, 6], peak_widths=(50,))
+fig, ax = plt.subplots()
+ax.scatter(spec['x'], spec['y'], s=4)
+for i in peaks_found:
+    ax.axvline(x=spec['x'][i], c='black', linestyle='dotted')
+
+def XRD_Fit(filename):
+  x,y = RasRead(filename)
+  
